ddcf63c60c3f40b1769afcabdb708e0546ed0c01
Noam Bavli <noambav@gmail.com>
2024-12-07 02:47:06 +0200

    the PDF is a liar ?

--- Scrabble.java
+++ Scrabble.java
@@ -145,9 +145,9 @@ public class Scrabble {
 				System.out.println("Invalid word. Try again.");
 		}
 		if (hand.length() == 0) {
-			System.out.println("Ran out of letters. Total score: " + score + " points\n");
+			System.out.println("Ran out of letters. Total score: " + score + " points");
 		} else {
-			System.out.println("End of hand. Total score: " + score + " points\n");
+			System.out.println("End of hand. Total score: " + score + " points");
 		}
 	}
 
